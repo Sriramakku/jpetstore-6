8740833e7bc067730f3a3c7ddf3086b5af3b4bf3
Sriramakku <57284755+Sriramakku@users.noreply.github.com>
2024-01-09 21:23:22 +0530

    updated jenkins file

--- main.tf
+++ main.tf
@@ -6,3 +6,21 @@ resource "aws_instance" "public_instance" {
    Name = var.name_tag,
  }
 }
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all-traffic"
+  description = "Security group allowing all traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "ami" {
 variable "instance_type" {
    type        = string
    description = "Instance type"
-   default     = "t3.micro"
+   default     = "t3.small"
 }
 
 variable "name_tag" {
